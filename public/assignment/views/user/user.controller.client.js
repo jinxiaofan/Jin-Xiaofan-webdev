cbb6adbd110079c53ef65c38d02d217ae55a9016
jin <jin.xia@husky.neu.edugit config --global user.email jin.xia@husky.neu.edu>
2016-10-25 23:21:10 -0700

    assignment3.1

--- public/assignment/views/user/user.controller.client.js
+++ public/assignment/views/user/user.controller.client.js
@@ -9,25 +9,34 @@
         var vm = this;
         vm.login = login;
 
-        function login(username, password) {
-            var user = UserService.findUserByCredentials(username, password);
-            if (user === null) {
-                vm.error = "No such user";
-            } else {
-                $location.url("/user/" + user._id);
+        function login(user) {
+            if (vm.user == null) {
+                vm.error = "No such user"
+            }
+            else {
+                var user = UserService.findUserByCredentials(vm.user.username, vm.user.password);
+                if (user === null) {
+                    vm.error = "No such user"
+                }
+                else {
+                    $location.url("/user/" + user._id);
+                }
             }
         }
     }
 
     function RegisterController(UserService, $location) {
         var vm = this;
-
         vm.createUser = createUser;
-
         function createUser(user) {
-            user._id = (new Date()).getTime();
-            UserService.createUser(user);
-            $location.url("/user/" + user._id);
+
+            if (user.password != user.password2) {
+                vm.error = "Wrong Password!"
+            } else {
+                user._id = (new Date()).getTime();
+                UserService.createUser(user);
+                $location.url("/user/" + user._id);
+            }
         }
     }
 
