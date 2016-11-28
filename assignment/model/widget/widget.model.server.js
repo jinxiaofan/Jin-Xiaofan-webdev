37769ecd2e1e22569ddd648ce6d28657caa46085
jin <jin.xia@husky.neu.edugit config --global user.email jin.xia@husky.neu.edu>
2016-11-27 21:59:42 -0800

    assignment4

---
+++ assignment/model/widget/widget.model.server.js
@@ -0,0 +1,74 @@
+module.exports = function() {
+    var mongoose = require("mongoose");
+    var WidgetSchema = require("./widget.schema.server")();
+    var WidgetModel = mongoose.model("WidgetModel", WidgetSchema);
+
+    var api = {
+        createWidget : createWidget,
+        findWidgetsByPageId: findWidgetsByPageId,
+        findWidgetById: findWidgetById,
+        updateWidget: updateWidget,
+        deleteWidget: deleteWidget
+    };
+    return api;
+
+
+    function createWidget(widget){
+        console.log(widget);
+        return WidgetModel.create(widget);
+    }
+
+    function findWidgetsByPageId(pageId) {
+        return WidgetModel.find({
+            "pageId" : pageId
+        })
+    }
+
+    function findWidgetById(wid) {
+        return WidgetModel.findById(wid);
+    }
+
+    function updateWidget(widgetId, widget) {
+        if(widget.rows == null){
+            widget.rows = 0;
+        }
+        if(widget.size == null){
+            widget.size = 0;
+        }
+        if(widget.deletable == null){
+            widget.deletable = true;
+        }
+
+        if(widget.formatted == null){
+            widget.formatted = true;
+        }
+        return WidgetModel.update({
+                _id: widgetId
+            },
+            {
+                "pageId": widget.pageId,
+                "widgetType": widget.widgetType,
+                "name": widget.name,
+                "text": widget.text,
+                "placeholder": widget.placeholder,
+                "description": widget.description,
+                "url": widget.url,
+                "width": widget.width,
+                "height": widget.height,
+                "rows": widget.rows,
+                "size": widget.size,
+                "class": widget.class,
+                "icon": widget.icon,
+                "deletable": widget.deletable,
+                "formatted": widget.formatted,
+                "dateCreated": Date.now()
+
+            });
+    }
+
+    function deleteWidget(widgetId) {
+        return WidgetModel
+            .remove({_id: widgetId});
+    }
+
+};
