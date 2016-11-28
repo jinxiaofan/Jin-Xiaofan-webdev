fed78726cb04854e5046e5fa4edc06b80b01ead0
jin <jin.xia@husky.neu.edugit config --global user.email jin.xia@husky.neu.edu>
2016-11-27 22:50:00 -0800

    assignment4

--- assignment/model/page/page.schema.server.js
+++ assignment/model/page/page.schema.server.js
@@ -5,9 +5,8 @@ module.exports = function () {
         "name" : { type: String, required: true},
         "title": String,
         "description" : String,
-        "widgets" : {type: mongoose.Schema.Types.ObjectId, ref: "Widget"},
+        "widgets" : {type: mongoose.Schema.Types.ObjectId, ref: "WidgetModel"},
         "dateCreated": { type: Date, default: Date.now }
-
     }, {collection: "page"});
     return PageSchema;
 };
