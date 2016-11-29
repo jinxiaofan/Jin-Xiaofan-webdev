e6e45ebf09aeca9d8307b11dbe64d98ea8111bce
jin <jin.xia@husky.neu.edugit config --global user.email jin.xia@husky.neu.edu>
2016-11-29 13:42:34 -0800

    assignment5.2

--- assignment/model/model.server.js
+++ assignment/model/model.server.js
@@ -1,8 +1,8 @@
-model.exports = function () {
+module.exports = function () {
     var mongoose =  require('mongoose');
     mongoose.Promise = global.Promise;
 
-    var userModle = require("./user/user.model.server")();
+    var userModel = require("./user/user.model.server")();
     var websiteModel = require("./website/website.model.server")();
     var pageModel = require("./page/page.model.server")();
     var widgetModel = require("./widget/widget.model.server")();
