8786877b659126ab91e59a1cd5dc1e36fa1fdda0
jin <jin.xia@husky.neu.edugit config --global user.email jin.xia@husky.neu.edu>
2016-11-29 14:49:43 -0800

    assignment5.2

--- assignment/model/page/page.model.server.js
+++ assignment/model/page/page.model.server.js
@@ -1,4 +1,4 @@
-model.exports = function () {
+module.exports = function () {
     var mongoose = require("mongoose");
     var PageSchema = require("./page.schema.server")();
     var PageModel = mongoose.model("PageModel", PageSchema);
