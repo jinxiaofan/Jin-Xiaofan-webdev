d19e91f3f91f68d090bb7b840037848469df152d
jin <jin.xia@husky.neu.edugit config --global user.email jin.xia@husky.neu.edu>
2016-12-04 00:08:23 -0800

    assignment5.4

--- public/assignment/services/flk.service.client.js
+++ public/assignment/services/flk.service.client.js
@@ -9,19 +9,13 @@
         };
         return api;
 
-        var key = "your-flickr-key";
-        var secret = "your-flickr-secret";
+        // my flikr key: 68712ea26ff0c663a6f168449b32f1cd
+        // my flikr cecret: 51b37eb23b43e5be
+        var key = "68712ea26ff0c663a6f168449b32f1cd";
+        var secret = "51b37eb23b43e5be";
         var urlBase = "https://api.flickr.com/services/rest/?method=flickr.photos.search&format=json&api_key=API_KEY&text=TEXT";
 
-        function searchPhotos(searchTerm){
-            urlBase.replace("API_KEY", key).replace("TEXT", searchTerm)
-                .success(function(){
-                    return $http.get(url);
-                })
-                .error(function(){
-
-                })
-        }
-
+        var url = urlBase.replace("API_KEY", key).replace("TEXT", searchTerm);
+        return $http.get(url);
     }
 });
