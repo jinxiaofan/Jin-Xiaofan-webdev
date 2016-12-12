48db7a6cdc2dec7d9758267654f20050c6c7f2db
jin <jin.xia@husky.neu.edugit config --global user.email jin.xia@husky.neu.edu>
2016-12-11 22:33:32 -0800

    project

--- project/app.js
+++ project/app.js
@@ -2,6 +2,5 @@ module.exports = function (app) {
     var models = require("./models/models.server")();
 
     require("./services/user.service.server.js")(app, models);
-    // require("./services/mealPlan.service.server.js")(app, models);
 
 };
